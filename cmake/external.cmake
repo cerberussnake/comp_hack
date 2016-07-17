6891bc19322d6d13176bb397f7c0c37bfefb62ef
COMP Omega <compomega@tutanota.com>
2016-07-17 11:08:40 -0600

    [ADD] Squirrel external project.

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -19,6 +19,90 @@
 INCLUDE(ExternalProject)
 
 ExternalProject_Add(
+    squirrel3
+
+    GIT_REPOSITORY https://github.com/comphack/squirrel3.git
+    GIT_TAG master
+
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/squirrel3
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_CXX_FLAGS=-std=c++11 -stdlib=libc++
+
+    # Dump output to a log instead of the screen.
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_INSTALL ON
+
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libsquirrel.a
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libsqstdlib.a
+)
+
+ExternalProject_Get_Property(squirrel3 INSTALL_DIR)
+
+SET(SQUIRREL_INCLUDE_DIRS "${INSTALL_DIR}/include")
+
+ADD_LIBRARY(squirrel STATIC IMPORTED)
+ADD_DEPENDENCIES(squirrel squirrel3)
+SET_TARGET_PROPERTIES(squirrel PROPERTIES IMPORTED_LOCATION
+    "${INSTALL_DIR}/lib/libsquirrel.a")
+
+ADD_LIBRARY(sqstdlib STATIC IMPORTED)
+ADD_DEPENDENCIES(sqstdlib squirrel3)
+SET_TARGET_PROPERTIES(sqstdlib PROPERTIES IMPORTED_LOCATION
+    "${INSTALL_DIR}/lib/libsqstdlib.a")
+
+ExternalProject_Add(
+    asio
+
+    GIT_REPOSITORY https://github.com/chriskohlhoff/asio.git
+    GIT_TAG asio-1-11-0
+
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/asio
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> "-DCMAKE_CXX_FLAGS=-std=c++11 -stdlib=libc++"
+
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+
+    # Dump output to a log instead of the screen.
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_INSTALL ON
+)
+
+ExternalProject_Get_Property(asio INSTALL_DIR)
+
+SET(ASIO_INCLUDE_DIRS "${INSTALL_DIR}/src/asio/asio/include")
+
+ExternalProject_Add(
+    tinyxml2-ex
+
+    GIT_REPOSITORY https://github.com/comphack/tinyxml2.git
+    GIT_TAG comp_hack
+
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/tinyxml2
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS=-std=c++11 -stdlib=libc++
+
+    # Dump output to a log instead of the screen.
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_INSTALL ON
+
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libtinyxml2.a
+)
+
+ExternalProject_Get_Property(tinyxml2-ex INSTALL_DIR)
+
+SET(TINYXML2_INCLUDE "${INSTALL_DIR}/include")
+
+ADD_LIBRARY(tinyxml2 STATIC IMPORTED)
+ADD_DEPENDENCIES(tinyxml2 tinyxml2-ex)
+SET_TARGET_PROPERTIES(tinyxml2 PROPERTIES IMPORTED_LOCATION
+    "${INSTALL_DIR}/lib/libtinyxml2.a")
+
+ExternalProject_Add(
     yamlcpp
 
     GIT_REPOSITORY https://github.com/comphack/yaml-cpp.git
