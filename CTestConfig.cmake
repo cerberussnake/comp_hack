9dc5ed3d9e8d0908eda5ed49bf6ea0860ca321dd
COMP Omega <compomega@tutanota.com>
2016-07-24 15:09:10 -0600

    [ADD] CDash configuration.

---
+++ CTestConfig.cmake
@@ -0,0 +1,24 @@
+# This file is part of COMP_hack.
+#
+# Copyright (C) 2010-2016 COMP_hack Team <compomega@tutanota.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set(CTEST_PROJECT_NAME "comp_hack")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=comp_hack")
+set(CTEST_DROP_SITE_CDASH TRUE)
