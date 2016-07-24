5af49350580c8134dbd15128d13b1d85a30efef7
COMP Omega <compomega@tutanota.com>
2016-07-24 16:36:57 -0600

    [ADD] Tester library.

---
+++ libtester/src/LobbyClient.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file libtester/src/LobbyClient.h
+ * @ingroup libtester
+ *
+ * @author COMP Omega <compomega@tutanota.com>
+ *
+ * @brief Class to create a lobby test connection.
+ *
+ * This file is part of the COMP_hack Tester Library (libtester).
+ *
+ * Copyright (C) 2012-2016 COMP_hack Team <compomega@tutanota.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "LobbyClient.h"
+
+using namespace libtester;
+
+LobbyClient::LobbyClient()
+{
+}
---
+++ libtester/tests/Lobby.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file libcomp/tests/DiffieHellman.cpp
+ * @ingroup libcomp
+ *
+ * @author COMP Omega <compomega@tutanota.com>
+ *
+ * @brief Test the Diffie-Hellman key exchange.
+ *
+ * This file is part of the COMP_hack Library (libcomp).
+ *
+ * Copyright (C) 2014-2016 COMP_hack Team <compomega@tutanota.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <PushIgnore.h>
+#include <gtest/gtest.h>
+#include <PopIgnore.h>
+
+#include <LobbyConnection.h>
+#include <Log.h>
+
+#include <thread>
+
+using namespace libcomp;
+
+TEST(Lobby, Connection)
+{
+    libcomp::Log::GetSingletonPtr()->AddStandardOutputHook();
+
+    asio::io_service service;
+
+    std::thread serviceThread([&service]()
+    {
+        service.run();
+    });
+
+    libcomp::LobbyConnection connection(service);
+    connection.Connect("127.0.0.1", 10666);
+
+    serviceThread.join();
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
