b83873a2d814cdaad37e02bf0fedf5ace4cc72fc
COMP Omega <compomega@tutanota.com>
2016-07-27 06:58:41 -0600

    [FIX] Catch more in the tests.

--- libcomp/tests/Decrypt.cpp
+++ libcomp/tests/Decrypt.cpp
@@ -229,11 +229,11 @@ int main(int argc, char *argv[])
     try
     {
         ::testing::InitGoogleTest(&argc, argv);
+
+        return RUN_ALL_TESTS();
     }
     catch(...)
     {
         return EXIT_FAILURE;
     }
-
-    return RUN_ALL_TESTS();
 }
